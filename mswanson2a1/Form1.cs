4dd56c07ba1a1aa072e5a439a82bf8d6e45964dc
Malachi Nelson <wu8424tn@go.minnstate.edu>
2019-09-29 15:02:48 -0500

    completed ex2a

---
+++ mswanson2a1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mswanson2a1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            decimal subtotal = Convert.ToDecimal(txtSubtotal.Text);
+            decimal discountPercent = Convert.ToDecimal(txtDiscountPercent.Text);
+            decimal discountAmount = discountPercent * subtotal / 100;
+            decimal total = subtotal - discountAmount;
+
+            txtDiscountAmount.Text = discountAmount.ToString("n2");
+            txtTotal.Text = total.ToString("n2");
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ variableTest/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace variableTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            //txtHotDogsSubtotal.Text = (
+            //        Convert.ToDecimal(txtHotDogs.Text) * 4.0m
+            //    ).ToString("0.00");
+            int hotDogs = Convert.ToInt32(txtHotDogs.Text);
+            decimal hotDogPrice = 4.0m;
+            decimal hotDogSubtotal = hotDogs * hotDogPrice;
+            txtHotDogsSubtotal.Text = hotDogSubtotal.ToString("n2");
+
+            //txtHamburgersSubtotal.Text = (
+            //        Convert.ToDecimal(txtHamburgers.Text) * 5.0m
+            //    ).ToString("0.00");
+            int hamburgers = Convert.ToInt32(txtHamburgers.Text);
+            decimal hamburgerPrice = 5.0m;
+            decimal hamburgerSubtotal = hamburgers * hamburgerPrice;
+            txtHamburgersSubtotal.Text = hamburgerSubtotal.ToString("n2");
+
+            //txtPretaxTotal.Text = (
+            //        Convert.ToDecimal(txtHotDogsSubtotal.Text) + Convert.ToDecimal(txtHamburgersSubtotal.Text)
+            //    ).ToString("0.00");
+            decimal pretaxTotal = hotDogSubtotal + hamburgerSubtotal;
+            txtPretaxTotal.Text = pretaxTotal.ToString("n2");
+
+            //txtTax.Text = (
+            //        Convert.ToDecimal(txtPretaxTotal.Text) * 0.06875m
+            //    ).ToString("0.00");
+            decimal tax = pretaxTotal * 0.06875m;
+            txtTax.Text = tax.ToString("n2");
+
+            //txtTotal.Text = (
+            //        Convert.ToDecimal(txtPretaxTotal.Text) + Convert.ToDecimal(txtTax.Text)
+            //    ).ToString("0.00");
+            //btnClear.Focus();
+            decimal total = pretaxTotal + tax;
+            txtTotal.Text = total.ToString("n2");
+            btnClear.Focus();
+            
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            txtHotDogs.Text = "0";
+            txtHamburgers.Text = "0";
+            txtHotDogsSubtotal.Text = "";
+            txtHamburgersSubtotal.Text = "";
+            txtPretaxTotal.Text = "";
+            txtTax.Text = "";
+            txtTotal.Text = "";
+
+            txtHotDogs.Focus();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
