6c9be60a90a8c4d0680f6197e301e8dfe888320a
Malachi Nelson <wu8424tn@go.minnstate.edu>
2019-11-06 18:50:53 -0600

    completed ex2g

---
+++ mswanson2g1/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace mswanson2g1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.input1aTextBox = new System.Windows.Forms.TextBox();
+            this.result01SwitchTextBox = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.resultIf01TextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.resultElseIf01TextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resultSwitchDefault01TextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.resultIfDefault01TextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.resultElseIfDefault01TextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.resultNestedIfElse01TextBox = new System.Windows.Forms.TextBox();
+            this.resultNestedIfElseDefault01TextBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.resultNestedIfElse02TextBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.resultElseIf02TextBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.resultIf02TextBox = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.result02SwitchTextBox = new System.Windows.Forms.TextBox();
+            this.input2aTextBox = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1) Switch R/C";
+            // 
+            // input1aTextBox
+            // 
+            this.input1aTextBox.Location = new System.Drawing.Point(162, 6);
+            this.input1aTextBox.Name = "input1aTextBox";
+            this.input1aTextBox.Size = new System.Drawing.Size(65, 20);
+            this.input1aTextBox.TabIndex = 1;
+            // 
+            // result01SwitchTextBox
+            // 
+            this.result01SwitchTextBox.Location = new System.Drawing.Point(256, 6);
+            this.result01SwitchTextBox.Name = "result01SwitchTextBox";
+            this.result01SwitchTextBox.Size = new System.Drawing.Size(77, 20);
+            this.result01SwitchTextBox.TabIndex = 2;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(162, 349);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(65, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // resultIf01TextBox
+            // 
+            this.resultIf01TextBox.Location = new System.Drawing.Point(256, 32);
+            this.resultIf01TextBox.Name = "resultIf01TextBox";
+            this.resultIf01TextBox.Size = new System.Drawing.Size(77, 20);
+            this.resultIf01TextBox.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "2) If R/C";
+            // 
+            // resultElseIf01TextBox
+            // 
+            this.resultElseIf01TextBox.Location = new System.Drawing.Point(256, 58);
+            this.resultElseIf01TextBox.Name = "resultElseIf01TextBox";
+            this.resultElseIf01TextBox.Size = new System.Drawing.Size(77, 20);
+            this.resultElseIf01TextBox.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "3) Else If R/C";
+            // 
+            // resultSwitchDefault01TextBox
+            // 
+            this.resultSwitchDefault01TextBox.Location = new System.Drawing.Point(256, 110);
+            this.resultSwitchDefault01TextBox.Name = "resultSwitchDefault01TextBox";
+            this.resultSwitchDefault01TextBox.Size = new System.Drawing.Size(77, 20);
+            this.resultSwitchDefault01TextBox.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(49, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "5) Switch w/default";
+            // 
+            // resultIfDefault01TextBox
+            // 
+            this.resultIfDefault01TextBox.Location = new System.Drawing.Point(256, 136);
+            this.resultIfDefault01TextBox.Name = "resultIfDefault01TextBox";
+            this.resultIfDefault01TextBox.Size = new System.Drawing.Size(77, 20);
+            this.resultIfDefault01TextBox.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(49, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "6) If R/C";
+            // 
+            // resultElseIfDefault01TextBox
+            // 
+            this.resultElseIfDefault01TextBox.Location = new System.Drawing.Point(256, 162);
+            this.resultElseIfDefault01TextBox.Name = "resultElseIfDefault01TextBox";
+            this.resultElseIfDefault01TextBox.Size = new System.Drawing.Size(77, 20);
+            this.resultElseIfDefault01TextBox.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(49, 165);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "7) Else If R/C";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "4) Nested if-else";
+            // 
+            // resultNestedIfElse01TextBox
+            // 
+            this.resultNestedIfElse01TextBox.Location = new System.Drawing.Point(256, 84);
+            this.resultNestedIfElse01TextBox.Name = "resultNestedIfElse01TextBox";
+            this.resultNestedIfElse01TextBox.Size = new System.Drawing.Size(77, 20);
+            this.resultNestedIfElse01TextBox.TabIndex = 10;
+            // 
+            // resultNestedIfElseDefault01TextBox
+            // 
+            this.resultNestedIfElseDefault01TextBox.Location = new System.Drawing.Point(256, 188);
+            this.resultNestedIfElseDefault01TextBox.Name = "resultNestedIfElseDefault01TextBox";
+            this.resultNestedIfElseDefault01TextBox.Size = new System.Drawing.Size(77, 20);
+            this.resultNestedIfElseDefault01TextBox.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(49, 191);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(83, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "8) Nested if-else";
+            // 
+            // resultNestedIfElse02TextBox
+            // 
+            this.resultNestedIfElse02TextBox.Location = new System.Drawing.Point(256, 292);
+            this.resultNestedIfElse02TextBox.Name = "resultNestedIfElse02TextBox";
+            this.resultNestedIfElse02TextBox.Size = new System.Drawing.Size(77, 20);
+            this.resultNestedIfElse02TextBox.TabIndex = 27;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(49, 295);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(83, 13);
+            this.label9.TabIndex = 26;
+            this.label9.Text = "4) Nested if-else";
+            // 
+            // resultElseIf02TextBox
+            // 
+            this.resultElseIf02TextBox.Location = new System.Drawing.Point(256, 266);
+            this.resultElseIf02TextBox.Name = "resultElseIf02TextBox";
+            this.resultElseIf02TextBox.Size = new System.Drawing.Size(77, 20);
+            this.resultElseIf02TextBox.TabIndex = 25;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(49, 269);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(71, 13);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "3) Else If R/C";
+            // 
+            // resultIf02TextBox
+            // 
+            this.resultIf02TextBox.Location = new System.Drawing.Point(256, 240);
+            this.resultIf02TextBox.Name = "resultIf02TextBox";
+            this.resultIf02TextBox.Size = new System.Drawing.Size(77, 20);
+            this.resultIf02TextBox.TabIndex = 23;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(49, 243);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(48, 13);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "2) If R/C";
+            // 
+            // result02SwitchTextBox
+            // 
+            this.result02SwitchTextBox.Location = new System.Drawing.Point(256, 214);
+            this.result02SwitchTextBox.Name = "result02SwitchTextBox";
+            this.result02SwitchTextBox.Size = new System.Drawing.Size(77, 20);
+            this.result02SwitchTextBox.TabIndex = 21;
+            // 
+            // input2aTextBox
+            // 
+            this.input2aTextBox.Location = new System.Drawing.Point(162, 214);
+            this.input2aTextBox.Name = "input2aTextBox";
+            this.input2aTextBox.Size = new System.Drawing.Size(65, 20);
+            this.input2aTextBox.TabIndex = 20;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(49, 217);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(74, 13);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "1) Switch R/C";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 398);
+            this.Controls.Add(this.resultNestedIfElse02TextBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.resultElseIf02TextBox);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.resultIf02TextBox);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.result02SwitchTextBox);
+            this.Controls.Add(this.input2aTextBox);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.resultNestedIfElseDefault01TextBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.resultElseIfDefault01TextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.resultIfDefault01TextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.resultSwitchDefault01TextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.resultNestedIfElse01TextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.resultElseIf01TextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.resultIf01TextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.result01SwitchTextBox);
+            this.Controls.Add(this.input1aTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "mswanson2g1 Switch, if-else";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox input1aTextBox;
+        private System.Windows.Forms.TextBox result01SwitchTextBox;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox resultIf01TextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox resultElseIf01TextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox resultSwitchDefault01TextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox resultIfDefault01TextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox resultElseIfDefault01TextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox resultNestedIfElse01TextBox;
+        private System.Windows.Forms.TextBox resultNestedIfElseDefault01TextBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox resultNestedIfElse02TextBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox resultElseIf02TextBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox resultIf02TextBox;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox result02SwitchTextBox;
+        private System.Windows.Forms.TextBox input2aTextBox;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ mswanson2g1UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("mswanson2g1UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("mswanson2g1UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dcbaf2a4-7d98-42e4-b00b-b463ae0773a6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
